436ce3e944563a7dddf08c70dedddd42712d2de0
keano voelkl <keanov@gmail.com>
2017-07-04 19:04:02 +0000

    can fetch shortened version of url given regular length url

--- express_server.js
+++ express_server.js
@@ -2,10 +2,23 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.set('view engine', 'ejs');
+
 var urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
+  '9sm5xK': 'http://www.google.com',
+  'adsf': 'www.google.ca'
 };
+
+app.get('/urls', function(req, res) {
+  let templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
+})
+
+app.get('/urls/:id', function (req, res) {
+  let templateVars = {shortURL: req.params.id, urls: urlDatabase};
+  res.render('urls_show', templateVars);
+});
 
 app.get('/', function(req, res) {
   res.end('Hello!');
