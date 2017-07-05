1c1831271433682104e03bac18a88f21b7d292b7
keano voelkl <keanov@gmail.com>
2017-07-05 19:14:34 +0000

    implemented login/logout feature

--- express_server.js
+++ express_server.js
@@ -80,6 +80,10 @@ app.post('/login', (req, res) => {
   res.redirect('/urls')
 });
 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls')
+});
 
 app.listen(PORT, function() {
   console.log(`Example app listening on port ${PORT}`);
